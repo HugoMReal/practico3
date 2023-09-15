113c8ee877ef90fc577966abee78e48581b26c73
HugoMReal <realhmaximiliano@gmail.com>
2023-09-15 00:27:01 -0300

    estilo boton

---
+++ src/componentes/StyleButton.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const DefaultButton = styled.button`
+   border-radius: 20px;
+   border: solid;
+   width: 100px;
+   font-size: 1.2rem;
+   align-items: center;
+  
+    
+`
